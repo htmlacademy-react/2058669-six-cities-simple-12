0e95e2a4b380d78ecdadeb10176c622bfa3c7618
CorisMorri <103724309+CorisMorri@users.noreply.github.com>
2023-03-06 15:19:35 +0300

    Создание компанентов

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const SettingPlaces = {PlacesCount: 312};
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      placesCount={SettingPlaces.PlacesCount}
+    />
   </React.StrictMode>,
 );
